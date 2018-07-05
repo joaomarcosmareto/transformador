46a1f9e1e2b8bca94bedd93cac6fde1952dda7fe
Ciro <ciro_xm@yahoo.com.br>
2018-07-04 21:58:27 -0300

    rascunho funcional de funcao que renderiza uma linha

--- index.php
+++ index.php
@@ -55,25 +55,104 @@ $modelo = '{
 $modelo = json_decode($modelo, true);
 $lines = file ('esqueleto/assets/js/controllers/ModeloController.js');
 
-$R = $modelo["caracter_replace"];
+$GLOBALS['R'] = $modelo["caracter_replace"];
 
-$retorno = "";
-foreach ($lines as $line_num => $line) {
-//    echo "Linha #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br>\n";
-    $lineTrim = trim($line);
-    if(($line_num === 1 && $lineTrim === $R."REPLACE"))
-    {
-        break;
-    }
+var_dump(renderLinha($lines[3], $modelo["entidades"][0]));
+
+//$retorno = "";
+//foreach ($lines as $line_num => $line) {
+////    echo "Linha #<b>{$line_num}</b> : " . htmlspecialchars($line) . "<br>\n";
+//    $lineTrim = trim($line);
+//    if(($line_num === 1 && $lineTrim === $R."REPLACE"))
+//    {
+//        break;
+//    }
+//    
+//    if(seNovoTrecho($lineTrim))
+//        processarTrecho($lines, $line_num, getFinalLineTrecho($lines, $line_num));
+//    else
+//        $result += $line;
+//}
+//
+//$lineTrim = trim($line);
+//if(($line_num === 1 && $lineTrim === $R."REPLACE"))
+//{
+//    return;
+//}
+
+function processarTrecho($lines, $line_num, $final_trecho_num){
     
-    //var_dump(strstr($lineTrim, $R."For:"));
+    $lineTrim = $lines[$line_num];
+    
     if(strstr($lineTrim, $R."For:")){
-        $ifor = explode(" ", $lineTrim)[1];//TODO ver se tem mais de um
-        echo $ifor;
-    }        
+        $ifor = explode(" ", $lineTrim)[1];
+        //echo $ifor;
+        getConteudo($lines, $line_num);
+        
+        foreach ($modelo[$ifor] as $i => $item) {
+            renderLinha($line, $ifor, $item);
+        }
+    }
 }
 
-//function getConteudo
+function renderLinha($line, $modelo){
+//    var_dump($modelo);
+    //var_dump($line);
+    //para cada $ $ da linha, fazer:
+    $elements = [];
+    preg_match_all("/.*§(.*)§.*/U", $line, $elements);
+    //var_dump($elements);
+    
+    foreach ($elements[1] as $toReplace) {
+
+        $line = str_replace('§'.$toReplace.'§', $modelo[$toReplace], $line);
+        
+        //TODO: parte com ponto
+//        $toReplaceArr = explode($toReplace, ".");        
+//        
+//        if(count($toReplaceArr) === 1){
+//            str_replace($R.$toReplace.$R, $modelo[$nome], $line);
+//        }elseif(count($toReplaceArr) === 2){
+//            str_replace($R.$toReplaceArr[0].$toReplaceArr[1].$R, $modelo[$toReplaceArr[0]][$toReplaceArr[1]], $line);
+//        }   
+    }
+    
+    return $line;
+}
+
+function xxx($lines, $index = 0){
+    $countLines = count($lines);
+    $lvl = 1;
+    for (;$index < $countLines;$index++) {
+        //var_dump(strstr($lineTrim, $R."For:"));
+        if(seNovoTrecho($lineTrim)){
+            $ifor = explode(" ", $lineTrim)[1];//TODO ver se tem mais de um
+            echo $ifor;
+            getConteudo($lines, $line_num);
+        }
+    }
+}
+
+function getFinalLineTrecho($lines, $index = 0){
+    $countLines = count($lines);
+    $lvl = 1;
+    for (;$index < $countLines;$index++) {
+        if(strstr($lineTrim, $R.$R)){
+            $lvl--;
+            if($lvl === 1){
+                return $index;
+            }
+        }
+        if(seNovoTrecho($lineTrim)){
+            $lvl++;
+        }
+    }
+    //TODO: se não achar nada o arquivo template ta errado, gerar exception
+}
+
+function seNovoTrecho($l){
+    return strstr($l, $R."For:") || strstr($l, $R."If:");
+}
 
 
 //$modelo = json_decode(file_get_contents('php://input'), true);
